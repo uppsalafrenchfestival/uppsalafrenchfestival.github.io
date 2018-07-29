e8e22c0c066d9a1494164c611c5d9d76a00108c2
uppsalafrenchfestival <gregory.vaumourin@it.uu.se>
2018-07-29 13:07:35 +0200

    update

--- js/tickets_form.js
+++ js/tickets_form.js
@@ -1,39 +1,136 @@
 
 function getTotal()
 {
-    var student_price = document.getElementById('student_price').checked;
-    var ticket_type = document.getElementById('ticket_type').value;
+    div = document.getElementById("ticket_order");
+    divs = div.getElementsByTagName('div');
+    
+    total = 0;
+    nb_tickets = 0;
+    for( var i = 0; i < divs.length; i++) 
+    {
+        var id = divs[i].id;
+        if( id == null)
+            continue;
 
-    if ( ticket_type == "price:festival-pass" ) 
+        split_line = id.split('_');
+        if(split_line.length != 2)
+            continue;
+
+        if(split_line[1] == "quantity")
+        {
+            ticket_type = split_line[0];
+            quantity = parseInt(divs[i].innerHTML);
+            price_seances = quantity * getPriceTicket(ticket_type); 
+            if(divs[i+1].id ==  ticket_type + "_price")
+            {
+                divs[i+1].innerHTML = price_seances + "SEK";
+
+            }
+            total += price_seances;
+            if( ticket_type != "festival-pass" )
+            {
+                nb_tickets += quantity;
+            }
+
+        }
+    }
+
+    if(nb_tickets >= 3)
+    {
+        price_reduction = Math.floor(nb_tickets / 3) * getPriceTicket("reduction"); 
+        reduction_display = document.getElementById("reduction_price");
+        reduction_display.innerHTML = price_reduction + "SEK";
+        total += price_reduction;
+    }
+    return total;
+}
+
+function getPriceTicket(ticket_type)
+{
+    var student_price = document.getElementById('student_price').checked;
+
+    if ( ticket_type == "festival-pass" ) 
     {
         if( student_price == true)
             value = 320;
         else
             value = 350;
     }
-    else if ( ticket_type == "price:1ticket") 
+    else if ( ticket_type == "reduction")
+    {
+        if( student_price == true)
+            value = -30;
+        else
+            value = -40;
+
+    }
+    else
     {
         if( student_price == true)
             value = 70;
         else
             value = 80;
     }
-    else if ( ticket_type == "price:3tickets")
-    {
-        if( student_price == true)
-            value = 180;
-        else
-            value = 200;
-    }
-    else
-        value = 0;
     return value;
 }
 
+function removeOrder()
+{
+
+}
+
+function seance_add()
+{
+    var selected_option = document.getElementById('ticket_type');
+
+    var ticket_type = selected_option.value;
+    var ticket_text = selected_option[selected_option.selectedIndex].text;
+
+    addOrder(ticket_type , ticket_text);
+}
+
+function addOrder(ticket_type, ticket_text)
+{
+ 
+
+    if(ticket_type == "none")
+        return;
+
+    var entry = document.getElementById(ticket_type);
+    if( entry == null)
+    {   
+        price_per_ticket = getPriceTicket(ticket_type);
+        code_to_add = "<div class=\"row\" id=\""+ ticket_type + "\">";
+        code_to_add += "<div class=\"col-sm-4\" id=\"" + ticket_type + "_text\"> " + ticket_text + " </div>";
+        code_to_add += "<div class=\"col-sm-1\" id=\"" + ticket_type + "_quantity\"> 1 </div>";
+        code_to_add += "<div class=\"col-sm-1\" id=\"" + ticket_type + "_price\" > "+ price_per_ticket+ "SEK </div> </div>";
+
+        $('#ticket_order').append(code_to_add);
+    }
+    else
+    {
+        var quantity_display = document.getElementById(ticket_type + '_quantity');
+        var price_display = document.getElementById(ticket_type + '_price');
+        if( quantity_display == null || price_display == null ){
+            alert("Error");
+            return;
+        }
+        nb_tickets = parseInt(quantity_display.innerHTML);        
+        nb_tickets++;
+        quantity_display.innerHTML = "" + nb_tickets;
+
+        price_per_ticket = getPriceTicket(ticket_type);
+        price = price_per_ticket * nb_tickets;
+        price_display.innerHTML =  "" + price + "SEK";
+    }
+    updateCheckoutValue();
+}
 
 function updateCheckoutValue()
 {
-    document.getElementById('checkout_button').textContent = "Checkout " + getTotal() + "kr";
+    var total = getTotal();
+    document.getElementById('checkout_tickets').textContent = "Checkout " + total + "kr";
+    document.getElementById('order_total').innerHTML = "Total: " + total + "kr";
 }
 
 function tickets_form_answer(data)
--- pages/tickets_en.md
+++ pages/tickets_en.md
@@ -1,6 +1,6 @@
 ---
 layout: layout_tickets
-title: Uppsala French FilmFestival 
+title: Uppsala French FilmFestival Ticket
 permalink: /en/tickets
 ref: tickets
 lang: en
